045f50a1afbdf9ab63eab07360e6cd84257666e8
Oleksii <user@example.com>
2025-05-28 02:23:24 +0300

    2

--- main.py
+++ main.py
@@ -10,62 +10,61 @@ class Field:
 
 # Клас для зберігання імені контакту. Обов'язкове поле.
 class Name(Field):
-    def __init__(self, name):
-        self.name = name
-
-class IncorrectPhoneNumber(Exception):
-    def __init__(self, message="Помилка формату номера телефону.Номер телефону повинен містити тільки цифри та дорівнювати 10 знакам"):
-        self.message = message
-        super().__init__(self.message)
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Ім'я не може бути порожнім.")
+        super().__init__(value)
 
 
 # Клас для зберігання номера телефону. Має валідацію формату (10 цифр).
 class Phone(Field):
-    '''
-    Реалізовано валідацію номера телефону (має бути перевірка на 10 цифр).
-    Наслідує клас Field. Значення зберігaється в полі value .
-    '''
-    # реалізація класу
-    def phone_number_validation(self, phone:str):
-        if len(phone)==10 and phone.isdigit():
-            return phone
+    def __init__(self, value):
+        if len(value)==10 and value.isdigit():
+            super().__init__(value)
         else:
-            raise IncorrectPhoneNumber
-
-
+            raise ValueError(f"{ValueError}\nНе вірний формат номера")
+    
+    def __eq__(self, other):
+        eq = (self.value == other.value)
+        return eq
 
 # Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів.
 class Record:
-    '''
-        Реалізовано зберігання об'єкта Name в атрибуті name.
-        Реалізовано зберігання списку об'єктів Phone в атрибуті phones.
-        Реалізовано метод для додавання - add_phone .На вхід подається рядок, який містить номер телефона.
-        Реалізовано метод для видалення - remove_phone. На вхід подається рядок, який містить номер телефона.
-        Реалізовано метод для редагування - edit_phone. На вхід подається два аргумента - рядки, які містять старий номер телефона та новий. 
-    У разі некоректності вхідних даних метод має завершуватись помилкою ValueError.
-        Реалізовано метод для пошуку об'єктів Phone - find_phone. На вхід подається рядок, який містить номер телефона. Метод має повертати або об’єкт Phone, або None .
-    '''
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
-
-    # реалізація класу
+   
     def add_phone(self, phone: str):
-        self.phones.append(Phone.phone_number_validation(phone))
+        #!!!!ChatGPT поради реалізувати через __eq__ в class(Phone) тому попередню реалізацію закоментив!!!
+        # triger=True                      
+        # for ithem in self.phones:
+        #     if str(ithem)==phone:
+        #         triger=False
+        # if triger:
+        #     self.phones.append(Phone(phone))
+        # else:
+        #     print(f'{phone} is already present in the notebook for {self.name}')
+        if Phone(phone) not in self.phones:
+            self.phones.append(Phone(phone))
+        else:
+            print(f'{phone} is already present in the notebook for {self.name}')
 
-    def remove_phone(self, phone: str):
-        if phone in self.phones:
-            self.phones.remove(phone)
+    def remove_phone(self, phone):
+        if Phone(phone) in self.phones:
+            self.phones.remove(Phone(phone))
         else:
             print('Номер не знайдено')
 
-    def edit_phone(self, old_phone: str, new_phone: str):
+    def edit_phone(self, old_phone, new_phone):
         try:
-            self.phones.remove(old_phone)
-            add_phone(new_phone)
+            self.phones.remove(Phone(old_phone))
+            self.add_phone(new_phone)
         except ValueError:
-            print(f'Номер {old_phone} не знайдено')
+            print(f'{ValueError}\nНомер {old_phone} який необхідно змінити не знайдено')
 
+    def find_phone(self, phone_for_search):
+        if Phone(phone_for_search) in self.phones:
+            return Phone(phone_for_search)
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -73,14 +72,49 @@ class Record:
 #Клас для зберігання та управління записами.
 class AddressBook(UserDict):
     '''
-        Має наслідуватись від класу UserDict.
-        Реалізовано метод add_record, який додає запис до self.data. 
+    1) Має наслідуватись від класу UserDict.
+    2) Реалізовано метод add_record, який додає запис до self.data. 
     Записи Record у AddressBook зберігаються як значення у словнику. 
     В якості ключів використовується значення Record.name.value.
-        Реалізовано метод find, який знаходить запис за ім'ям. 
+    4) Реалізовано метод find, який знаходить запис за ім'ям. 
     На вхід отримує один аргумент - рядок, якій містить ім’я. Повертає об’єкт Record, або None, якщо запис не знайден.
     Реалізовано метод delete, який видаляє запис за ім'ям.
     Реалізовано магічний метод __str__ для красивого виводу об’єкту класу AddressBook .
     '''
-    # реалізація класу
-    pass
+    def add_record(self, value):
+        key = Record.value.name.value
+        print(f"Key in add_record {key}")
+        value = Record(value)
+        print(f"value in add_record {value}")
+        self.data[key] = value
+    
+    def __str__(self):
+        return "!!!!".join(f"{k}<> {v}" for k, v in self.data.items())
+
+book = AddressBook()
+bob = Record("Bob")
+# print(f"Створено об'єкт {bob}")
+bob.add_phone("1234567890")
+# print (f"виконано bob.add_phone('1234567890')\n{bob}")
+bob.add_phone("0004567890")
+# print (f"виконано bob.add_phone('0004567890')\n{bob}")
+# bob.add_phone("0004567890")
+# print (f"виконано bob.add_phone('0004567890')\n{bob}")
+# print (f"Перевірка remove_phone")
+# bob.remove_phone('1234567890')
+# print (f"виконано bob.remove_phone('1234567890')\n{bob}")
+# bob.remove_phone('1234567890')
+# print (f"виконано bob.remove_phone('1234567890')\n{bob}")
+# print('Перевірка edit')
+# bob.edit_phone('1234567890','0004567890')
+# print (f"виконано bob.edit_phone('1234567890','0004567890')\n{bob}")
+# bob.edit_phone('0004567890','1234567000')
+# print (f"виконано bob.edit_phone('0004567890','1234567000')\n{bob}")
+# print(bob.find_phone('1234567000'))
+# print (f"виконано print(bob.find_phone('1234567000'))\n{bob}")
+# print(bob.find_phone('5555555555'))
+# print (f"виконано print(bob.find_phone('5555555555'))\n{bob}")
+# print(bob.name.value)
+book.add_record(bob)
+print(book)
+print(bob.name.value)
